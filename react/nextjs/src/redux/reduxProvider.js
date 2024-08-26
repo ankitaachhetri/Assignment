706f30c353c5f4fdaae8378f5a9dded28c909998
ankitaakc <156278868+ankitaakc@users.noreply.github.com>
2024-08-26 07:15:46 +0545

    added redux

--- react/nextjs/src/redux/reduxProvider.js
+++ react/nextjs/src/redux/reduxProvider.js
@@ -1,3 +1,4 @@
+'use client'
 import React, { Children } from 'react'
 import { Provider } from 'react-redux'
 import store from './store'
--- react/nextjs/src/redux/store.js
+++ react/nextjs/src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterSlice from './reducerSlice/counterSlice'
 
 export default configureStore({
-  reducer: {}
+  reducer: {
+  counter: counterSlice
+  }
 })
