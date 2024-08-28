92f58a8514a1ddc4b27635fa02714cabf7a350b0
ankitaakc <156278868+ankitaakc@users.noreply.github.com>
2024-08-28 07:48:58 +0545

    issue

--- react/nextjs/src/redux/store.js
+++ react/nextjs/src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './reducerSlice/counterSlice'
-import boxSlice from './reducerSlice/boxSlice'
+import { boxSlice } from './reducerSlice/boxSlice'
+
 
 export default configureStore({
   reducer: {
-  counter: counterSlice
-  box:boxSlice
+  counter: counterSlice,
+  box: boxSlice
   }
 })
